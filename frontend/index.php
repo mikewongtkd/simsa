6af92e47f3636168b0c3feeed5cbd1b3740b8b99
Mike Wong <mikewongtkd@gmail.com>
2024-07-17 15:23:36 -0700

    Adding front page

--- frontend/index.php
+++ frontend/index.php
@@ -13,7 +13,7 @@
     <meta name="author" content="">
     <link rel="icon" href="/docs/4.0/assets/img/favicons/favicon.ico">
 
-    <title>Cover Template for Bootstrap</title>
+    <title>Simsa - Promotion Test Management Software</title>
 
     <link rel="canonical" href="https://getbootstrap.com/docs/4.0/examples/cover/">
 
@@ -22,6 +22,10 @@
 
     <!-- Custom styles for this template -->
     <link href="include/css/simsa/cover.css" rel="stylesheet">
+
+    <script src="include/jquery-3.7.1/jquery.min.js"></script>
+    <script src="include/popperjs-2.11.8-dist/popper.min.js"></script>
+    <script src="include/bootstrap-5.3.3-dist/js/bootstrap.min.js"></script>
   </head>
 
   <body class="text-center">
@@ -29,7 +33,7 @@
     <div class="cover-container d-flex h-100 p-3 mx-auto flex-column">
       <header class="masthead mb-auto">
         <div class="inner">
-          <h3 class="masthead-brand">Cover</h3>
+          <h3 class="masthead-brand">Simsa 심사</h3>
           <nav class="nav nav-masthead justify-content-center">
             <a class="nav-link active" href="#">Home</a>
             <a class="nav-link" href="#">Features</a>
@@ -39,8 +43,8 @@
       </header>
 
       <main role="main" class="inner cover">
-        <h1 class="cover-heading">Cover your page.</h1>
-        <p class="lead">Cover is a one-page template for building simple and beautiful home pages. Download, edit the text, and add your own fullscreen background photo to make it your own.</p>
+        <h1 class="cover-heading">Simsa 심사</h1>
+        <p class="lead"><b>Simsa</b> is a Taekwondo belt promotion examination (승품단심사) management system following <a class="kkw-ref" href="#" data-toggle="tooltip" title="<i>Poom-Dan Promotion Test Examiner Course Textbook</i><br>Kukkiwon World Taekwondo Academy (June 2022)<br>ISBN 978-89-93484-43-4">Kukkiwon procedures</a>.</p>
         <p class="lead">
           <a href="#" class="btn btn-lg btn-secondary">Learn more</a>
         </p>
@@ -48,19 +52,16 @@
 
       <footer class="mastfoot mt-auto">
         <div class="inner">
-          <p>Cover template for <a href="https://getbootstrap.com/">Bootstrap</a>, by <a href="https://twitter.com/mdo">@mdo</a>.</p>
+          <p> &copy;<?= date( 'Y' ) ?> Mike Wong. All rights reserved.</p>
         </div>
       </footer>
     </div>
 
-
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
-    <script src="include/popper-2.11.8-dist/popper.min.js"></script>
-    <script src="include/bootstrap-5.3.3-dist/js/bootstrap.min.js"></script>
   </body>
+  <script>
+    $(() => {
+      $('[data-toggle="tooltip"]').tooltip({ html : true })
+    })
+  </script>
 </html>
 
